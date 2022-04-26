7b85df343f36feccce3e9c4fe14ec4a18bda0007
anompermadi <anom.permadi@outlook.com>
2022-04-26 10:22:56 +0700

    filter logic

--- script/main.js
+++ script/main.js
@@ -1,4 +1,46 @@
-const todoList = [];
+const todoList = ["learn javascript", "learn node js"];
+
+function clearTodolist(){
+    const todolistBody = document.getElementById("todolistBody");
+    while(todolistBody.firstChild){
+        todolistBody.removeChild(todolistBody.firstChild);
+    }
+}
+
+function addTodolist(todo){
+
+    const tr = document.createElement("tr");
+    const tdButton = document.createElement("td");
+    tr.appendChild(tdButton);
+
+    const buttonDone = document.createElement("input");
+    buttonDone.type = "button";
+    buttonDone.value = "Done";
+    tdButton.appendChild(buttonDone);
+
+    const tdTodo = document.createElement("td");
+    tdTodo.textContent = todo;
+    tr.appendChild(tdTodo);
+
+    const todolistBody = document.getElementById("todolistBody");
+    todolistBody.appendChild(tr);
+}
+
+function displayTodolist(){
+    
+    clearTodolist()
+
+    for (let i = 0; i < todoList.length; i++){
+        const todo = todoList[i];
+        
+        const searchText = document.getElementById("search").value.toLowerCase();
+
+        if(todo.toLowerCase().includes(searchText)){
+            addTodolist(todo);
+        }
+        
+    }
+}
 
 document.forms["todoForm"].onsubmit = function (event){
     event.preventDefault();
@@ -8,4 +50,16 @@ document.forms["todoForm"].onsubmit = function (event){
 
     document.forms["todoForm"].reset();
     console.info(todoList);
+    displayTodolist();
 };
+
+const searchInput = document.getElementById("search");
+
+searchInput.onkeyup = function (){
+    displayTodolist();
+}
+searchInput.onkeydown = function (){
+    displayTodolist();
+}
+
+displayTodolist();
